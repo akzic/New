232bd17e6768f46113ee1b686ab6ffe09eb51e1f
akzic <137202213+akzic@users.noreply.github.com>
2023-07-20 05:34:13 +0900

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,32 +1,50 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/edit.css') }}">
-    </head>
-    <body>
+@extends('layouts.app')
+
+@section('title', 'Edit Product')
+
+@section('content')
         <form id="product-form" action="{{ route('update', ['id' => $product->id]) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <h1>商品情報編集画面</h1>
-            <label for="id">ID</label><br>
-            <input type="text" id="id" name="id" value="{{ $product->id }}" readonly><br>
+            <!-- 商品名 -->
             <label for="name">商品名</label><br>
-            <input type="text" id="name" name="name" value="{{ $product->product_name }}"><br>
-            <label for="company_id">メーカーID</label><br>
-            <input type="text" id="company_id" name="company_id" value="{{ $product->company->id }}" readonly><br>
+            <input type="text" id="name" name="name" placeholder="商品名を入力してください" value="{{ old('name', $product->product_name) }}"><br>
+            @if ($errors->has('name'))
+                <div class="error-message">{{ $errors->first('name') }}</div>
+            @endif
+
+            <!-- メーカー名 -->
             <label for="manufacturer">メーカー名</label><br>
-            <input type="text" id="manufacturer" name="manufacturer" value="{{ $product->company->company_name }}"><br>
+            <input type="text" id="manufacturer" name="manufacturer" placeholder="メーカー名を入力してください" value="{{ old('manufacturer', $product->company->company_name) }}" required><br>
+            @if ($errors->has('manufacturer'))
+                <div class="error-message">{{ $errors->first('manufacturer') }}</div>
+            @endif
+
+            <!-- 価格 -->
             <label for="price">価格</label><br>
-            <input type="number" id="price" name="price" value="{{ $product->price }}"><br>
+            <input type="number" id="price" name="price" placeholder="価格を入力してください" value="{{ old('price', $product->price) }}"><br>
+            @if ($errors->has('price'))
+                <div class="error-message">{{ $errors->first('price') }}</div>
+            @endif
+
+            <!-- 在庫数 -->
             <label for="stock">在庫数</label><br>
-            <input type="number" id="stock" name="stock" value="{{ $product->stock }}"><br>
+            <input type="number" id="stock" name="stock" placeholder="在庫数を入力してください" value="{{ old('stock', $product->stock) }}"><br>
+            @if ($errors->has('stock'))
+                <div class="error-message">{{ $errors->first('stock') }}</div>
+            @endif
+
+            <!-- コメント -->
             <label for="comment">コメント</label><br>
-            <textarea id="comment" name="comment">{{ $product->comment }}</textarea><br>
+            <textarea id="comment" name="comment" placeholder="コメントを入力してください">{{ old('comment', $product->comment) }}</textarea><br>
+            @if ($errors->has('comment'))
+                <div class="error-message">{{ $errors->first('comment') }}</div>
+            @endif
             <label for="image">商品画像</label><br>
             <input type="file" id="image" name="image"><br>
             <input type="submit" value="更新">
             <button id="back-button">戻る</button>
         </form>
         <script src="{{ asset('js/edit.js') }}"></script>
-    </body>
-</html>
+@endsection
