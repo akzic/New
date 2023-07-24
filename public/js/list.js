fc2ececdf0835fe9c7a1033b458802690bc8f97f
akzic <137202213+akzic@users.noreply.github.com>
2023-07-25 00:42:22 +0900

    Update list.js

--- public/js/list.js
+++ public/js/list.js
@@ -12,3 +12,20 @@ function addProduct() {
   let url = 'http://localhost:8888/New/public/add/';
   window.location.href = url;
 }
+
+function searchProducts() {
+  const searchInput = document.getElementById('search-input').value;
+  const manufacturerSelect = document.getElementById('manufacturer-select').value;
+  let url = 'http://localhost:8888/New/public/list?';
+
+  // キーワードとメーカー名のパラメータをURLに追加
+  if (searchInput) {
+      url += 'keyword=' + encodeURIComponent(searchInput) + '&';
+  }
+  if (manufacturerSelect) {
+      url += 'manufacturer=' + encodeURIComponent(manufacturerSelect);
+  }
+
+  // リダイレクト
+  window.location.href = url;
+}
