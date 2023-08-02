d01727aca8f948b7874063ce924318ec3d49c9ac
akzic <137202213+akzic@users.noreply.github.com>
2023-08-02 22:47:07 +0900

    Update NewController.php

--- app/Http/Controllers/NewController.php
+++ app/Http/Controllers/NewController.php
@@ -177,48 +177,4 @@ class NewController extends Controller
         }
     }
 
-
-    public function purchase(Request $request)
-    {
-        $productId = $request->get('product_id');
-        $quantity = $request->get('quantity');
-
-        // Start transaction
-        DB::beginTransaction();
-
-        try {
-            // Get product
-            $product = Product::find($productId);
-
-            if (!$product) {
-                return response()->json(['error' => 'Product not found'], 404);
-            }
-
-            // Check product stock
-            if ($product->stock < $quantity) {
-                return response()->json(['error' => 'Not enough stock for this product'], 400);
-            }
-
-            // Decrease product stock
-            $product->stock -= $quantity;
-            $product->save();
-
-            // Create new sale record
-            $sale = new Sale();
-            $sale->product_id = $productId;
-            $sale->quantity = $quantity;
-            $sale->save();
-
-            // Commit transaction
-            DB::commit();
-
-            return response()->json(['success' => 'Purchase completed successfully'], 200);
-        } catch (\Exception $e) {
-            // Rollback transaction
-            DB::rollBack();
-
-            return response()->json(['error' => 'Purchase failed: ' . $e->getMessage()], 500);
-        }
-    }
-
 }
