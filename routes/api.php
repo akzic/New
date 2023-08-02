0b03f51a30231fce2ec17ee66abfcb5c9168c46a
akzic <137202213+akzic@users.noreply.github.com>
2023-08-02 22:44:13 +0900

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\SalesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/purchase', [SalesController::class, 'purchase'])->name('purchase');
