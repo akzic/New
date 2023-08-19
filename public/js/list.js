44d9a96ea0248040645f9316718842eae9180b4c
akzic <137202213+akzic@users.noreply.github.com>
2023-08-19 17:45:19 +0900

    Update list.js

--- public/js/list.js
+++ public/js/list.js
@@ -12,7 +12,6 @@ function addProduct() {
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('search-button').addEventListener('click', searchProducts);
-  
   document.getElementById('min-price').addEventListener('input', searchProducts);
   document.getElementById('max-price').addEventListener('input', searchProducts);
   document.getElementById('min-stock').addEventListener('input', searchProducts);
@@ -21,25 +20,50 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function searchProducts(event) {
   if (event) event.preventDefault();
-
   const searchParams = buildSearchParams();
-  fetchProductList(`${BASE_URL}/list?${searchParams.toString()}`);
+  fetchProductList(`${BASE_URL}/list/search?${searchParams.toString()}`);
 }
 
 function fetchProductList(url) {
   $.ajax({
-    url: url,
-    type: 'GET',
-    success: function(data) {
-      const productListContainer = document.getElementById('product-list');
-      productListContainer.innerHTML = ''; // Clear existing content
-      productListContainer.insertAdjacentHTML('beforeend', data); // Insert new HTML content
-    },
-    error: function(error) {
-      console.error('Error fetching products:', error);
-    }
+      url: url,
+      type: 'GET',
+      success: function(data) {
+          const productListContainer = document.getElementById('product-list');
+          productListContainer.innerHTML = ''; 
+
+          if (data.products && Array.isArray(data.products)) {
+              data.products.forEach(product => {
+                  let productRowHtml = `
+                      <tr id="product-row-${product.id}">
+                          <td>${product.id}</td>
+                          <td><img src="${BASE_URL}/storage/images/${product.img_path}" alt="商品画像"></td>
+                          <td>${product.product_name}</td>
+                          <td>${product.price}</td>
+                          <td>${product.stock}</td>
+                          <td>${product.company.company_name}</td>
+                          <td>
+                              <button onclick="showDetail('${product.id}')">詳細</button>
+                          </td>
+                          <td>
+                              <button type="button" onclick="deleteProduct(event, ${product.id})">削除</button>
+                          </td>
+                      </tr>
+                  `;
+                  productListContainer.insertAdjacentHTML('beforeend', productRowHtml); 
+              });
+          } else {
+              // レスポンスが想定されるJSON形式でない場合、直接テーブルコンテナに追加
+              const tableContainer = document.getElementById('table-container');
+              tableContainer.innerHTML = data;
+          }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          console.error('Error fetching products:', errorThrown, jqXHR.responseText);
+      }
   });
 }
+
 
 let currentSortField = 'id';
 let currentSortOrder = 'asc';
@@ -54,7 +78,6 @@ function buildSearchParams() {
 
   const searchParams = new URLSearchParams();
 
-  // Append search parameters
   if (searchInput) {
       searchParams.append('keyword', searchInput);
   }
@@ -73,8 +96,7 @@ function buildSearchParams() {
   if (maxStock) {
       searchParams.append('maxStock', maxStock);
   }
-
-  // Append sort parameters
+  
   searchParams.append('sort', currentSortField);
   searchParams.append('order', currentSortOrder);
 
@@ -84,7 +106,7 @@ function buildSearchParams() {
 function deleteProduct(event, id) {
   event.preventDefault();
 
-  if (!confirm('商品を削除しますか？')) {  // 確認ダイアログ
+  if (!confirm('商品を削除しますか？')) {
     return;
   }
 
@@ -97,7 +119,7 @@ function deleteProduct(event, id) {
     },
     success: function(data) {
       if (data.success) {
-        document.querySelector(`#product-row-${id}`).remove();  // remove product row from the table
+        document.querySelector(`#product-row-${id}`).remove();
       } else {
         alert('商品の削除に失敗しました。');
       }
@@ -109,12 +131,31 @@ function deleteProduct(event, id) {
   });
 }
 
-
 function sortProductsByField(field) {
-  // Update the current sort field and reverse the sort order
   currentSortField = field;
   currentSortOrder = currentSortOrder === 'desc' ? 'asc' : 'desc';
-
-  // Trigger a new search with the updated sort parameters
   searchProducts();
 }
+
+$(document).ready(function() {
+  $('#search-form').on('submit', function(e) {
+      e.preventDefault();
+
+      $.ajax({
+          url: `${BASE_URL}/list/search`,
+          type: 'POST',
+          data: $(this).serialize(),
+          headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+          },
+          success: function(data) {
+              $('#table-container').html(data);
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+              console.error('Error:', errorThrown);
+              alert('検索に失敗しました。');
+          }
+      });
+  });
+});
